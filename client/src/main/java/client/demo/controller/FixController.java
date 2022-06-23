3f07b202df157c7268f99481f7b1097539932c78
yichen17 <q07218396@163.com>
2022-06-23 23:09:23 +0800

    1、修改数据库密码 被攻击了，删了数据库让我用0.015比特币换，我笑了 2、修复数据 2022-06-10 至 2022-06-23 的数据，前开后闭

---
+++ client/src/main/java/client/demo/controller/FixController.java
@@ -0,0 +1,15 @@
+package client.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Qiuxinchao
+ * @version 1.0
+ * @date 2022/6/23 22:54
+ * @describe 修补错误数据controller
+ */
+@RestController
+@Slf4j
+public class FixController {
+}
